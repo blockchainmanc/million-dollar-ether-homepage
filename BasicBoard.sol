314f4092a97e0596f716c196055c7b5c8d32e61a
James Morgan <james.morgan.e@googlemail.com>
2017-09-10 21:04:34 +0100

    Adding basic million dollar pixel board

--- BasicBoard.sol
+++ BasicBoard.sol
@@ -18,43 +18,41 @@ pragma solidity ^0.4.0;
  **/
 contract BasicBoard {
 
-	struct Owner {
-		uint32 purchaseAtTime;
-		uint ownerID;
-	}
-	mapping(address => Owner) private owners;
-	mapping(uint => address) private ownerAddresses;
-
-	struct Area {
-		address owner; // who owns that pixel
-		uint colour; // the colour hex code
-		
-		// coordinates
+	struct Coordinates {
 		uint8 fromX;
 		uint8 toX;
-		uint8 fromY;
+		uint8 fromY; 
 		uint8 toY;
 	}
 
-	Area[100][100] private areasPurchased;
+	struct Owner {
+		uint32 purchasedDateTime;
+		Coordinates coordinates; // the purchased coordinates
+	}
+
+	// a map of address to owner
+	mapping(address => Owner) private owners;
+
+	struct Area {
+		string colour; // the colour hex code
+		address owner; // who owns that pixel
+	}	
+
+	// Events
+	event Purchased(uint8 fromX, uint8 toX, uint8 fromY, uint8 toY, string colour, address owner);
+	event Error(uint8 fromX, uint8 toX, uint8 fromY, uint8 toY, string reason);
 
 	// Board Config
-	
+	Area[100][100] public board;
+
 	address private boardCreator;
 	
 	// emergency kill switch, disables all future sales
-	bool private stopped = false; 
+	bool public stopped = false; 
 	
 	function BasicBoard() {
 		// keep a track of the board creator
 		boardCreator = msg.sender;
-
-		// create an initial owner for the board creator
-		owners[boardCreator].purchaseAtTime = uint32(now);
-		owners[boardCreator].ownerID = 0;
-
-		// persist mapping from owner to address
-		ownerAddresses[owners[boardCreator].ownerID] = boardCreator;
 	}
 
 	// Accessors
@@ -68,11 +66,50 @@ contract BasicBoard {
 		// iterate the pixel space
 		for (uint8 ix = fromX; ix <= toX; ix++) {
 			for (uint8 iy = fromY; iy <= toY; iy++) {
-				// TODO assign colour to AREA array			
+				
+				// Look up the existing area block
+				Area storage existingBlock = board[ix][iy];
+
+				// validate area is not owned or is owned by caller
+				require(existingBlock.owner == 0 || existingBlock.owner == msg.sender);
+
+				// TODO how to handle changes by block owner ?
+
+				// Set block to colour at location
+				board[ix][iy] = Area(colour, msg.sender);
+
+				// Define the coordinates
+				// Coordinates memory coordinates = Coordinates(fromX, toX, fromY, toY);
+
+				// simple keep a map of owner to datetime and coordinates
+				// owners[msg.sender] = Owner(uint32(now), coordinates);
 			}
 		}
+
+		Purchased(fromX, toX, fromY, toY, colour, msg.sender);
+
 		return true;
 	}
+
+	// Short hand for querying specific coordinate
+	function getPixelDetails(uint x, uint8 y) constant returns (string colour, address owner) {   
+		Area storage area = board[x][y];
+		return (area.colour, area.owner);
+    }
+
+	// User can check what space they own
+	function getOwner() constant returns (uint32 purchasedDateTime, uint8 fromX, uint8 toX, uint8 fromY, uint8 toY) {   
+		// TODO - this feels hacky - any better ways to check if you own any space?
+		// TODO - how to handle the same user buy 
+		Owner storage owner = owners[msg.sender];
+		return (
+			owner.purchasedDateTime, 
+			owner.coordinates.fromX, 
+			owner.coordinates.toX, 
+			owner.coordinates.fromY, 
+			owner.coordinates.toY
+		);
+    }
 
 	function toggleContractActive() isBoardAdmin public {
 		stopped = !stopped;
